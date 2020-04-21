fbe41484a40579ab598f57fd159ff2d7b760ec9f
jrindnls <jrindnls@rams.colostate.edu>
2020-04-20 20:48:45 -0600

    ~/fuel/app/views/hospitalviews

---
+++ hospital_files/about.php
@@ -0,0 +1,40 @@
+<?php
+//echo Asset::img("", array("alt" => "Justin Daniels", "class" => "center"));
+//
+////echo Asset::img("raghd.jpg", array("alt" => "Raghd Alowairdhi", "class" => "center"));
+//
+//
+?>
+
+<h1 style="text-align:center;">Justin Daniels</h1>
+<?php
+echo Asset::img("Justin.jpg", array("alt" => "Justin Daniels", "height" => "200", "width" => "200", "class" => "center"));
+?>
+<!-- <img src="Justin.jpg" alt="justin" height="42" width="42"> -->
+<p style="text-align:center;"> ACT Student </p>
+<p style="text-align:center;"> jrindnls@rams.colostate.edu </p>
+<p style="text-align: center">Link to <a href="https://www.cs.colostate.edu:4444/~jrindnls/ct310/index.php/ourhospital/home.php">Justin's</a> website.</p>
+<hr class="w3-border-grey" style="margin:auto;width:40%">
+
+<h1 style="text-align:center;">Sara Williams</h1>
+<?php
+echo Asset::img("sara.jpg", array("alt" => "Sara Williams", "height" => "200", "width" => "200", "class" => "center"));
+?>
+<!-- <img src="sara.jpg" alt="sara " height="42" width="42"> -->
+<p style="text-align:center;"> ACT Student </p>
+<p style="text-align:center;"> @rams.colostate.edu </p>
+<p style="text-align: center">Link to <a href="https://www.cs.colostate.edu:4444/~jrindnls/ct310/index.php/ourhospital/home.php">Sara's</a> website.</p>
+<hr class="w3-border-grey" style="margin:auto;width:40%">
+
+<h1 style="text-align:center;">Raghd Alowairdhi</h1>
+<?php
+echo Asset::img("raghd.jpg", array("alt" => "Raghd Alowairdhi", "height" => "200", "width" => "200", "class" => "center"));
+?>
+<!-- <img src="raghd.jpg" alt="raghd " height="42" width="42"> -->
+<p style="text-align:center;"> ACT Student </p>
+<p style="text-align:center;"> raghd@rams.colostate.edu </p>
+<p style="text-align: center">Link to <a href="https://www.cs.colostate.edu:4444/~raghd/ct310/index.php/ourhospital/home.php">Raghd's</a> website.</p>
+<hr class="w3-border-grey" style="margin:auto;width:40%">
+<p  style="text-align:center;" >This is our team: Raghad Alowairdhi, Justin Daniels, and Sara Williams.</p>
+
+
