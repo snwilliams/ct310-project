acafc58cd9ad1a47f5b9346e997685f9b4a0b906
Sara Williams <sara.williams.n@gmail.com>
2020-04-20 20:01:34 -0600

    add css styling

--- fuel/app/views/hospitalviews/about.php
+++ fuel/app/views/hospitalviews/about.php
@@ -4,3 +4,19 @@ echo Asset::img("Justin.jpg", array("alt" => "Justin Daniels", "class" => "cente
 ?>
 
 <p>This is our team: Raghad Alowairdhi, Justin Daniels, and Sara Williams.</p>
+
+<div class="card">
+ <?php echo Asset::img("Justin.jpg", array("alt" => "Justin Daniels", "class" => "center")); ?>
+  <div class="container">
+    <h4><b>Justin Daniels</b></h4>
+    <p>About Justin</p>
+  </div>
+</div>
+
+<div class="card">
+  <img src="Justin.jpg" alt="Justin Daniels" style="width:100%">
+  <div class="container">
+    <h4><b>Justin Daniels</b></h4>
+    <p>About Justin</p>
+  </div>
+</div>
