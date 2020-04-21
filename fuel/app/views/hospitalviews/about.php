3762bb810316c493f058295ba67e45cf7b1deb6d
Sara Williams <sara.williams.n@gmail.com>
2020-04-20 20:28:55 -0600

    add images for Raghd and Sara

--- fuel/app/views/hospitalviews/about.php
+++ fuel/app/views/hospitalviews/about.php
@@ -1,11 +1,11 @@
 <?php
 echo Asset::img("logo.png", array("alt" => "RJS Solutions logo", "class" => "center"));
-echo Asset::img("Justin.jpg", array("alt" => "Justin Daniels", "class" => "center"));
 ?>
+
 
 <p>This is our team: Raghad Alowairdhi, Justin Daniels, and Sara Williams.</p>
 
-<div class="card">
+<div class="card" style="floast: left; padding: 10px;" >
  <?php echo Asset::img("Justin.jpg", array("alt" => "Justin Daniels", "class" => "center")); ?>
   <div class="container">
     <h4><b>Justin Daniels</b></h4>
@@ -13,10 +13,18 @@ echo Asset::img("Justin.jpg", array("alt" => "Justin Daniels", "class" => "cente
   </div>
 </div>
 
-<div class="card">
-  <img src="Justin.jpg" alt="Justin Daniels" style="width:100%">
+<div class="card" style="floast: left; padding: 10px;" >
+  <?php echo Asset::img("sara.jpg", array("alt" => "Sara Williams", "class" => "center")); ?>
   <div class="container">
-    <h4><b>Justin Daniels</b></h4>
-    <p>About Justin</p>
+    <h4><b>Sara Williams</b></h4>
+    <p>About Sara</p>
+  </div>
+</div>
+
+<div class="card" style="floast: left; padding: 10px;" >
+  <?php echo Asset::img("raghd.jpg", array("alt" => "Raghd Alowairdhi", "class" => "center")); ?>
+  <div class="container">
+    <h4><b>Raghd Alowairdhi</b></h4>
+    <p>About Raghd</p>
   </div>
 </div>
