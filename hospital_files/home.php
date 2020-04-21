b1c09c698a973047893fa2814b3bf3641b438a9a
jrindnls <jrindnls@rams.colostate.edu>
2020-04-20 20:49:25 -0600

    ~/fuel/app/views/hospitalviews

---
+++ hospital_files/home.php
@@ -0,0 +1,13 @@
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  <div class="w3-display-topleft w3-padding-large w3-xlarge">
+      <?php
+      echo Asset::img("rjssol.png", array("alt" => "RJS Solutions", "height" => "300", "width" => "300", "class" => "center"));
+      ?>>
+  </div>
+  <div class="w3-display-middle">
+    <h1 class="w3-jumbo w3-animate-top"> Welcome to RJS Solutions</h1>
+    <hr class="w3-border-grey" style="margin:auto;width:40%">
+    <p class="w3-large w3-center"style =" color:black"> Home of the finest sourced Medicare data from 2017 around!  Striving to meet your data analysis needs.</p>
+  </div>
+
+</div>
