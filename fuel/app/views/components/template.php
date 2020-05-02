550d76b3f4ba42978b13ce9a8c2eab6195c22244
Sara Williams <sara.williams.n@gmail.com>
2020-05-02 13:33:36 -0600

    testing styles

--- fuel/app/views/components/template.php
+++ fuel/app/views/components/template.php
@@ -15,59 +15,22 @@
         }
         ?>
     </title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js"></script>
-<!--    --><?php
-//    echo Asset::js('jquery.tablesorter.js');
-//    echo Asset::js('jquery.tablesorter.widgets.js');
-//    echo Asset::js('widget-alignChar.js');
-//    echo Asset::js('widget-build-table.js');
-//    echo Asset::js('widget-columnSelector.js');
-//    echo Asset::js('widget-cssStickyHeaders.js');
-//    echo Asset::js('widget-editable.js');
-//    echo Asset::js('widget-grouping.js');
-//    echo Asset::js('widget-headerTitles.js');
-//    echo Asset::js('widget-math.js');
-//    echo Asset::js('widget-output.js');
-//    echo Asset::js('widget-pager.js');
-//    echo Asset::js('widget-print.js');
-//    echo Asset::js('widget-reflow.js');
-//    echo Asset::js('widget-repeatheaders.js');
-//    echo Asset::js('widget-scroller.js');
-//    echo Asset::js('widget-staticRow.js');
-//    ?>
-
-        <?php echo Asset::css('hospitalstyles/main.css'); ?>
+    
+    <?php echo Asset::css('hospitalstyles/main.css'); ?>
 </head>
 
 <body>
-<div>
-
-<?php include "navbar.php"; ?>
-<?php
-    if (isset($main_body)){
-        echo $main_body;
-    }
-    else {
-        echo "<h1>This page is under construction</h1>";
-    }
-
-
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
+    <div>
+        <?php include "navbar.php"; ?>
+        <?php
+            if (isset($main_body)){
+                echo $main_body;
+            }
+            else {
+                echo "<h1>This page is under construction</h1>";
+            }
+        ?>
+    </div>
+    <p>This is a test!</p>
 </body>
 </html>
