82ef206dec9d4c205940dc02a385872b2c398d7a
Sara Williams <sara.williams.n@gmail.com>
2020-05-02 13:45:54 -0600

    test navbar styles

--- fuel/app/views/components/template.php
+++ fuel/app/views/components/template.php
@@ -16,12 +16,40 @@
         ?>
     </title>
 
-    <?php echo Asset::css('main.css'); ?>
+    <?php echo Asset::css("main.css"); ?>
 </head>
 
 <body>
     <div>
-        <?php include "navbar.php"; ?>
+        <nav>
+        <ul>
+            <li>
+                <?php
+                $link = Uri::base() . "index.php/ourhospital/home.php";
+                echo "<a href='$link'>Home</a>";
+                ?>
+            </li>
+            <li>
+                <?php
+                $link = Uri::base() . "index.php/ourhospital/about.php";
+                echo "<a href='$link'>About us</a>";
+                ?>
+            </li>
+            <li>
+                <?php
+                $link = Uri::base() . "index.php/ourhospital/hospital_list.php";
+                echo "<a href='$link'>List of Hospitals</a>";
+                ?>
+            </li>
+            <li>
+                <?php
+                $link = Uri::base() . "index.php/ourhospital/drg_list.php";
+                echo "<a href='$link'>List of DRGs</a>";
+                ?>
+            </li>
+        </ul>
+        </nav>
+        
         <?php
             if (isset($main_body)){
                 echo $main_body;
