46e4d63ff31fa5c7151c18e371e3c5f99cc4af97
Sara Williams <sara.williams.n@gmail.com>
2020-05-06 15:00:37 -0600

    resized images

--- fuel/app/views/hospitalviews/about.php
+++ fuel/app/views/hospitalviews/about.php
@@ -1,28 +1,29 @@
 
 <?php echo Asset::css("about.css"); ?>
 
-<p>This is our team: Raghad Alowairdhi, Justin Daniels, and Sara Williams.</p>
+<div class="main">
+  <p>This is our team: Raghad Alowairdhi, Justin Daniels, and Sara Williams.</p>
+  <div class="card">
+  <?php echo Asset::img("Justin.jpg", array("alt" => "Justin Daniels", "class" => "center")); ?>
+    <div class="container">
+      <h4><b>Justin Daniels</b></h4>
+      <p>About Justin</p>
+    </div>
+  </div>
 
-<div class="card" style="floast: left; padding: 10px;" >
- <?php echo Asset::img("Justin.jpg", array("alt" => "Justin Daniels", "class" => "center")); ?>
-  <div class="container">
-    <h4><b>Justin Daniels</b></h4>
-    <p>About Justin</p>
+  <div class="card">
+    <?php echo Asset::img("sara.jpg", array("alt" => "Sara Williams", "class" => "center")); ?>
+    <div class="container">
+      <h4><b>Sara Williams</b></h4>
+      <p>About Sara</p>
+    </div>
+  </div>
+
+  <div class="card">
+    <?php echo Asset::img("raghd.jpg", array("alt" => "Raghd Alowairdhi", "class" => "center")); ?>
+    <div class="container">
+      <h4><b>Raghd Alowairdhi</b></h4>
+      <p>About Raghd</p>
   </div>
 </div>
-
-<div class="card" style="floast: left; padding: 10px;" >
-  <?php echo Asset::img("sara.jpg", array("alt" => "Sara Williams", "class" => "center")); ?>
-  <div class="container">
-    <h4><b>Sara Williams</b></h4>
-    <p>About Sara</p>
-  </div>
-</div>
-
-<div class="card" style="floast: left; padding: 10px;" >
-  <?php echo Asset::img("raghd.jpg", array("alt" => "Raghd Alowairdhi", "class" => "center")); ?>
-  <div class="container">
-    <h4><b>Raghd Alowairdhi</b></h4>
-    <p>About Raghd</p>
-  </div>
-</div>
+</main>
