faab9fc2af5645dd80ba5c5fc9c24648e579c0eb
Sara Williams <sara.williams.n@gmail.com>
2020-05-08 11:40:23 -0600

    what am i even doing

--- local_html/m2/assets/js/template.js
+++ local_html/m2/assets/js/template.js
@@ -1,6 +1,5 @@
 $('document').ready(() => {
     $("table").tablesorter({
-        theme: 'metro-dark',
       sortList: [[0,0],[1,0],[2,0]],
       widthFixed: true,
       widgets: ['zebra', 'columns', 'filter']
