bc7358021084d39d37dca186a83e1e0d103d9bff
Sara Williams <sara.williams.n@gmail.com>
2020-05-08 11:47:56 -0600

    styling?

--- fuel/app/views/hospitalviews/hospital_list.php
+++ fuel/app/views/hospitalviews/hospital_list.php
@@ -1,11 +1,7 @@
-<div class="row">
-    <div class="col-12">
-        <table id="hospitalList" class="tablesorter table">
-            <thead class="head-row">
-            <tr class="dark-row">
-                <th colspan="6" class="sorter-false">
-                    <h2 class="hl-title">LIST OF HOSPITALS</H2>
-                </th>
+<div>
+    <div>
+        <table id="hospitalList" class="table tablesorter">
+            <thead class="tableHead">
             <tr>
                 <th>Medicare Provider Number</th>
                 <th>Provider Name</th>
@@ -17,47 +13,72 @@
             </thead>
             <tbody>
             <?php
+            $count = 0;
             foreach($hospital_list as $row){
+                $count += 1;
                 echo "<tr>\n";
-                $i = 0;
-                foreach($row as $item) {
-                    if ($i ==0) {
-                        $uri = Uri::base() . "index.php/ourhospital/hospital_details.php?id=" . $row['provider_id'];
-                        $link = '<a href="' . $uri . '">' . $item . "</a>";
-                        
-                        echo "<td>" . $link . "</td>\n";
-                                             
-                    } else {
-                        echo "<td>" . $item . "</td>\n";
-                    }
-                    $i++;
-                }
+                $link = Uri::base() . "index.php/ourhospital/hospital_details.php?id=" . $row['provider_name'] . "&num=" . $row['provider_id'];
+                echo "<td>" . $row['provider_id'] . "</td>\n<td><a href='$link'>" . $row['provider_name'] . "</a></td>\n<td>" . $row['provider_street_address'] . "</td>\n
+                      <td>" . $row['provider_city'] . "</td>\n<td>" . $row['provider_state'] . "</td>\n<td>" . $row['hospital_referral_region'] . "</td>\n";
                 echo "</tr>\n";
             }
-            ?>
-            </tbody>
-        </table>
-        </div>
-        </div>
 
-        <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4">
-            <nav aria-label="Hospital Table Pagination">
-            <ul class="pagination justify-content-sm-center">
-                <?php
-                    if ($start > 0) {
-                        $prev_path=Uri::base() . "index.php/ourhospital/hospital_list/" . max($start - 25, 0);
-                        echo '<li class="page-item"><a class="page-link" href="' . $prev_path . '">Previous</a></li>';
-                    }                    
-                ?>
 
-                <?php 
-                    $next_path = Uri::base() . "index.php/ourhospital/hospital_list/" . ($start + 25);
-                    echo '<li class="page-item"><a class="page-link" href="' . $next_path . '">Next</a></li>';
-                ?>
-            </ul>
-            </nav>
-            </div>
-            <div class="col-4"></div>
-            </div>
+            echo "</tbody>";
+        echo "</table>";
+
+                if ($offset > 0){
+                    $previous = Uri::base() . "index.php/ourhospital/hospital_list/" . max($offset - 20, 0);
+                    echo "<a href=" . $previous . ">Previous 20 entries</a>";
+                    echo "<br>";
+                }if($count >= 20) {
+                $next = Uri::base() . "index.php/ourhospital/hospital_list/" . ($offset + 20);
+                echo "<a href=" . $next . ">Next 20 entries</a>";
+            }
+        ?>
+
+
+        <script>
+
+            $(function() {
+                $("table").tablesorter({
+                    theme: "ice",
+                    widthFixed: false,
+                    cancelSelection: true,
+                    tabIndex: true,
+                    usNumberFormat: true,
+                    serverSideSorting: false,
+                    resort: true,
+                    ignoreCase: true,
+                    sortForce: null,
+                    sortList: [
+                        [0,0],
+                        [1,0],
+                        [2.0],
+                        [3,0],
+                        [4,0],
+                        [5,0]
+                    ],
+                    sortInitialOrder: "asc",
+                    sortReset: true,
+                    initWidgets: true,
+                    widgets: ['zebra', 'columns', 'uitheme'],
+                    widgetOptions: {
+                        columns: [
+                            "DRG_Number",
+                            "DRG_Description"
+                        ],
+                        columns_thead: true,
+                        resizable: true,
+                        zebra: [
+                            "ui-widget-content even",
+                            "ui-state-default odd"
+                        ]
+                    },
+
+
+                });
+            });
+        </script>
+    </div>
+</div>
