6b3221d940003f34bd2bd9a874e01f2cb1826998
Sara Williams <sara.williams.n@gmail.com>
2020-05-07 22:00:09 -0600

    column name fix

--- fuel/app/views/hospitalviews/hospital_list.php
+++ fuel/app/views/hospitalviews/hospital_list.php
@@ -20,10 +20,10 @@
                     if ($i ==0) {
                         $link = Uri::base() . "index.php/ourhospital/hospital_details.php?id=" . $row['provider_name'];
                         echo "<td>" . $row['provider_id'] . "</td>\n<td><a href='$link'>" . $row['provider_name'] . "</a></td>\n<td>" . $row['provider_street_address'] . "</td>\n
-                              <td>" . $row['provider_city'] . "</td>\n<td>" . $row['provider_state'] . "</td>\n<td>" . $row['hospital_referral_region'] . "</td>\n";                     
+                              <td>" . $row['provider_city'] . "</td>\n<td>" . $row['provider_state'] . "</td>\n<td>" . $row['hospital_referral_region_hrr_description'] . "</td>\n";                     
                     } else {
                         echo "<td>" . $row['provider_id'] . "</td>\n<td><a href='$link'>" . $row['provider_name'] . "</a></td>\n<td>" . $row['provider_street_address'] . "</td>\n
-                              <td>" . $row['provider_city'] . "</td>\n<td>" . $row['provider_state'] . "</td>\n<td>" . $row['hospital_referral_region'] . "</td>\n"; 
+                              <td>" . $row['provider_city'] . "</td>\n<td>" . $row['provider_state'] . "</td>\n<td>" . $row['hospital_referral_region_hrr_description'] . "</td>\n"; 
                     }
                     $i++;
                 }
