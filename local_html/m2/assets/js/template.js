9acddae68dbeb7d05cbd1209d3fb868d190f67c8
Sara Williams <sara.williams.n@gmail.com>
2020-05-08 12:04:37 -0600

    please work

--- local_html/m2/assets/js/template.js
+++ local_html/m2/assets/js/template.js
@@ -1,12 +1,3 @@
 $('document').ready(() => {
-    $("table").tablesorter({
-      sortList: [[0,0],[1,0],[2,0]],
-      widthFixed: true,
-      widgets: ['zebra', 'columns', 'filter']
-    })
-    .tablesorterPager({
-      container: $(".pager"),
-      output: '{startRow} to {endRow} ({totalRows})'
-
-    });
+    $("table").tablesorter();
 })
