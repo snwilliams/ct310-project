e38deabfe970340315b1040b86b49cf76248d08b
Sara Williams <sara.williams.n@gmail.com>
2020-05-08 21:00:29 -0600

    register.php

--- fuel/app/views/hospitalviews/register.php
+++ fuel/app/views/hospitalviews/register.php
@@ -2,38 +2,29 @@
 echo Form::open(array(
     'action' => '/index.php/ourhospital/register',
     'method' => 'post',
-  
+    'class' => 'needs-validation',
+    'novalidate' => 'true'
 ));
 // Add security token
 echo \Form::csrf();
 ?>
-<div align="center">
 
+    <!-- Using bootstrap forms to make it pretty -->
+    <div class='form-group'>
+        <label for="username">Username</label>
+        <input type='text' class='form-control' id='username' name='username'  required>
+    </div>
+    <div class="form-group">
+        <label for="emailInput">Email address</label>
+        <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" name='email'  required>
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label for="passwordInput">Password</label>
+        <input type="password" class="form-control" id="passwordInput" name='password' required>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
 
-<h2>Register</h2>
-<!-- Using bootstrap forms to make it pretty -->
-<div class='form-group'>
-    <label for="username">Username</label>
-    <input type='text' class='form-control' id='username' name='username'  required>
-</div>
-<div class="form-group">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-<br>    <label for="email">Email address</label>
-    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name='email'  required>
-</div>
-<div class="form-group">
-    <label for="password">Password</label>
-    <input type="password" class="form-control"  name='password' required>
-</div>
-
-<div class="form-group">
-    <label for="confirm">Confirm Password</label>
-    <input type="password" class="form-control" name='confirm' required>
-</div>
-
-
-<button type="submit" class="btn btn-primary">Sign Up </button>
-</div>
 <?php
 echo Form::close();
 ?>
